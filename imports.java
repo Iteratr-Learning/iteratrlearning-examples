7db9ebe6047dc0eec59c13e34f1cf234807687d6
Richard Warburton <richard.warburton@gmail.com>
2016-11-20 23:28:06 +0000

    add more import examples

--- imports.java
+++ imports.java
@@ -1,2 +1,30 @@
 import java.util.stream.*;
 import static java.util.Map.*;
+import static java.util.stream.Collectors.*;
+
+class Payment
+{
+    private final int value;
+
+    public Payment(final int value)
+    {
+        this.value = value;
+    }
+
+    public int getValue()
+    {
+        return value;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Payment{" +
+            "value=" + value +
+            '}';
+    }
+}
+
+List<Payment> paymentsByValue =
+	Stream.of(900, 700, 500, 300, 100, 0).map(Payment::new).collect(toList());
+
