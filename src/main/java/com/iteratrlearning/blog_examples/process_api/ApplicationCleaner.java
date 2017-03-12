bd8a956f63c64a0a177befb55325357e29c65656
Richard Warburton <richard.warburton@gmail.com>
2017-03-12 12:39:05 +0000

    Cleanup code for blog post

--- src/main/java/com/iteratrlearning/blog_examples/process_api/ApplicationCleaner.java
+++ src/main/java/com/iteratrlearning/blog_examples/process_api/ApplicationCleaner.java
@@ -7,29 +7,24 @@ public class ApplicationCleaner
         cleanupApplication("chrome");
     }
 
-    private static void cleanupApplication(final String applicationName)
+    private static void cleanupApplication(String applicationName)
     {
         ProcessHandle.allProcesses()
                      .filter(process -> isApplication(applicationName, process))
                      .forEach(process ->
-                     {
                          process.info().command().ifPresent(command ->
-                         {
-                             System.out.println("Killing ... " + command);
-                             if (process.destroyForcibly())
-                             {
-                                 System.out.println("Success!");
-                             }
-                             else
-                             {
-                                 System.out.println("Failed");
-                             }
-                         });
-                     });
+                             closeAndLog(process, command)));
     }
 
-    private static boolean isApplication(final String chrome, final ProcessHandle process)
+    private static
+    void closeAndLog(ProcessHandle process, String command)
     {
-        return process.info().command().filter(command -> command.contains(chrome)).isPresent();
+        String status = process.destroyForcibly() ? " Success!" : " Failed";
+        System.out.println("Killing ... " + command + status);
+    }
+
+    private static boolean isApplication(final String appName, final ProcessHandle process)
+    {
+        return process.info().command().filter(command -> command.contains(appName)).isPresent();
     }
 }
